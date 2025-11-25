8fb1a2ac5d3d07a1e9d567a89e3190b63efb9b71
adityaraj61522 <adityaraj61522@gmail.com>
2025-11-26 02:37:32 +0530

    Fix app

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const doctorAuthMiddleware = require('./middleware/doctorAuthMiddleware');
-const assistantAuthMiddleware = require('./middleware/assistantAuthMiddleware');
-const { logRequest, logResponse } = require('./middleware/requestLogger');
+const doctorAuthMiddleware = require('./src/middleware/doctorAuthMiddleware');
+const assistantAuthMiddleware = require('./src/middleware/assistantAuthMiddleware');
+const { logRequest, logResponse } = require('./src/middleware/requestLogger');
 
 const app = express();
 
@@ -18,8 +18,7 @@ app.use(logResponse);
 app.use('/api/v1/doctor', doctorAuthMiddleware.verifyDoctorJWT);
 app.use('/api/v1/assistant', assistantAuthMiddleware.verifyAssistantJWT);
 
-require('./routes/routes')(app);
+require('./src/routes/routes')(app);
 
 const PORT = process.env.PORT || 6600;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
-
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: "b_ruby_ai_app",        // Name shown in PM2 list
-            script: "src/index.js",           // Your main entry file
+            script: "app.js",           // Your main entry file
             instances: 1,                  // Or 'max' to use all cores
             exec_mode: "fork",             // Or 'cluster' for load balancing
             watch: false,                  // Change to true for dev mode
